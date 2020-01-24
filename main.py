70346489eb78e39393dc3de6c3dba4aedac39838
Laurance18 <denizisik2004@gmail.com>
2020-01-24 19:13:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-import requests
-import os
+
+def main():
+    try:
+        subr = sys.argv[1]
+    except IndexError:
+        print("Please follow the instructions from the README file")
+
+    page = requests.get(f"https://www.reddit.com/r/{subr}/top/",stream=True,headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = html.fromstring(page.content)
+
+    imgs = webpage.xpath('//img[@alt="Post image"]/@src')
+
+
+if __name__ == "__main__":
+    import requests
+    import os
+    import sys
+    from lxml import html
+    main()
